e54e39ecc00010eb3ee36a2f22d3d22c43fbcb29
Emanuel M Borges <emanuel.eborges@gmail.com>
2023-03-06 15:17:53 -0400

    flutter sqlite mvc crud contatos

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:apisqlite/database/connection_contact.dart';
+import 'package:flutter/material.dart';
+import 'package:apisqlite/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
